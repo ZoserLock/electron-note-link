c3b45ad83b156c8b39b29a10b96a93cc5f10f403
Pablo Riedemann <zoserx@gmail.com>
2017-08-15 18:14:13 -0300

    Initial Commit - functional Madness

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+// Npm Modules
+import { app, BrowserWindow } from 'electron'
+
+class Application
+{
+    private _app:Electron.App;
+    private _mainWindow:Electron.BrowserWindow;
+
+    constructor(app:Electron.App)
+    {
+        this._app = app;
+        this._app.on('ready',()=>this.initialize());
+        this._app.on('window-all-closed', ()=>this.quit());
+    }
+
+    public initialize():void
+    {
+        this.createMainWindow();
+    }
+
+    public activate():void
+    {
+        if(this._mainWindow==null)
+        {
+            this.createMainWindow();
+        }
+    }
+
+    public quit():void
+    {
+        if (process.platform !== 'darwin') 
+        {
+            this._app.quit();
+        }
+    }
+
+    public createMainWindow():void
+    {
+        console.log(process.cwd());
+
+        this._mainWindow = new BrowserWindow({width: 1024, height: 768});
+
+        this._mainWindow.loadURL('file://'+__dirname+'/index.html');
+
+        this._mainWindow.on('closed', () => 
+        {
+            this._mainWindow = null
+        });
+    }
+
+}
+
+// Create Application
+const application = new Application(app);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+const commonConfig = 
+{
+    module: 
+    {
+        rules: 
+        [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: ['ts-loader']
+            }
+        ]
+    },
+    node: 
+    {
+        __dirname: false
+    },
+    resolve: 
+    {
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        modules: 
+        [
+            path.join(__dirname, 'app'),
+            'node_modules'
+        ]
+    },
+    output: 
+    {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    }
+}
+
+module.exports = [
+    Object.assign(
+    {
+        target: 'electron-main',
+        entry: { main: './src/main.ts' }
+    },
+    commonConfig),
+    Object.assign(
+    {
+        target: 'electron-renderer',
+        entry: { gui: './src/gui.tsx' },
+    },
+    commonConfig)
+  ]
